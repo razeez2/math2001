514529456630891d9a1eb65155767721f90a51b2
Daniel Groves <daniel.p.groves@gmail.com>
2024-08-23 12:35:46 -0500

    Deleting content of HW files after hw0.  Try to delete files later?

--- Math2001/Homework/hw6.lean
+++ Math2001/Homework/hw6.lean
@@ -12,33 +12,3 @@ set_option pp.funBinderTypes true
 Don't forget to compare with the text version,
 https://github.com/hrmacbeth/math2001/wiki/Homework-6,
 for clearer statements and any special instructions. -/
-
-
-@[autograded 4]
-theorem problem1 (P Q : Prop) : (P ∨ Q) ↔ (Q ∨ P) := by
-  sorry
-
-@[autograded 5]
-theorem problem2 (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
-  sorry
-
-@[autograded 5]
-theorem problem3 (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
-  sorry
-
-@[autograded 3]
-theorem problem4 : ¬ (∀ x : ℝ, x ^ 2 ≥ x) := by
-  push_neg
-  sorry
-
-@[autograded 4]
-theorem problem5 : ¬ Int.Even 7 := by
-  dsimp [Int.Even]
-  push_neg
-  sorry
-
-@[autograded 4]
-theorem problem6 {p : ℕ} (k : ℕ) (hk1 : k ≠ 1) (hkp : k ≠ p) (hk : k ∣ p) : ¬ Prime p := by
-  dsimp [Prime]
-  push_neg
-  sorry
--- Math2001/Homework/hw8.lean
+++ Math2001/Homework/hw8.lean
@@ -10,12 +10,3 @@ math2001_init
 Don't forget to compare with the text version,
 https://github.com/hrmacbeth/math2001/wiki/Homework-8,
 for clearer statements and any special instructions. -/
-
-
-@[autograded 4]
-theorem problem3 (m : ℤ) (hn : 66 ∣ 41 * m) : 66 ∣ m := by
-  sorry
-
-@[autograded 6]
-theorem problem4 (a : ℤ) : 3333 ∣ a ↔ (101 ∣ a ∧ 33 ∣ a) := by
-  sorry
