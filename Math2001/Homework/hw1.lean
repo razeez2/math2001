20e324d3466603637f728232842f0679e4fb87f3
Daniel Groves <daniel.p.groves@gmail.com>
2024-08-29 11:34:23 -0500

    Fixing hw1

--- Math2001/Homework/hw1.lean
+++ Math2001/Homework/hw1.lean
@@ -12,22 +12,22 @@ available as 215HW1.pdf on blackboard.
  -/
 
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem1 {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 := by
   sorry
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem2 {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by
   sorry
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem3 {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
   sorry
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem4 {x : ℚ} : x ^ 2 - 2 * x ≥ -1 := by
   sorry
 
-@[autograded 5]
+@[autogradedProof 5]
 theorem problem5 (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
   sorry
